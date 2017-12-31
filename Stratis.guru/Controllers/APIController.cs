cbd5a02678f04995091139a047a54f695f79ab8f
Clint Mourlevat <clint.mourlevat@outlook.fr>
2018-01-01 00:19:10 +0100

    Ajoutez des fichiers projet.

---
+++ Stratis.guru/Controllers/APIController.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stratis.guru.Controllers
+{
+    public class APIController : Controller
+    {
+        public async System.Threading.Tasks.Task<ActionResult> Price()
+        {
+            using (var client = new HttpClient())
+            {
+                dynamic StratisTicket = JsonConvert.DeserializeObject(await client.GetStringAsync("https://api.coinmarketcap.com/v1/ticker/stratis/"));
+                return Content(Convert.ToString(StratisTicket[0].price_usd));
+            }
+        }
+
+        public double ConvertEUR_USD()
+        {
+            using (var client = new WebClient())
+            {
+                dynamic StratisTicket = JsonConvert.DeserializeObject(client.DownloadString("https://api.fixer.io/latest"));
+                return Convert.ToDouble(StratisTicket.rates.USD);
+            }
+        }
+
+        public ActionResult PriceUSD()
+        {
+            return Content("dsds");
+        }
+
+        public ActionResult PriceEUR()
+        {
+            return Content("dsds");
+        }
+        public async System.Threading.Tasks.Task<ActionResult> PercentChange(string delay)
+        {
+            using (var client = new HttpClient())
+            {
+                dynamic StratisTicket = JsonConvert.DeserializeObject(await client.GetStringAsync("https://api.coinmarketcap.com/v1/ticker/stratis/"));
+                switch(delay)
+                {
+                    case "1h":
+                        return Content(Convert.ToString(StratisTicket[0].percent_change_1h));
+                    default:
+                    case "24h":
+                        return Content(Convert.ToString(StratisTicket[0].percent_change_24h));
+                    case "d":
+                        return Content(Convert.ToString(StratisTicket[0].percent_change_7d));
+                }
+            }
+        }
+    }
+}
---
+++ Stratis.guru/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+using NBitcoin;
+
+namespace Stratis.guru.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Active = "Index";
+            return View();
+        }
+        
+        [Route("generate-address", Name = "Generator")]
+        public ActionResult Generator()
+        {
+            ViewBag.Active = "Generator";
+            Key privateKey = new Key();
+            ViewBag.Pri = privateKey.GetWif(Network.StratisMain).ToString();
+            ViewBag.Pub = privateKey.PubKey.GetAddress(Network.StratisMain).ToString();
+            return View();
+        }
+
+        [ChildActionOnly]
+        /*[OutputCache(Duration = 60, VaryByParam = "none")]*/
+        public ActionResult HomePrice()
+        {
+            string CurrencySymbol = new RegionInfo(System.Threading.Thread.CurrentThread.CurrentCulture.LCID).ISOCurrencySymbol;
+            using (var client = new WebClient())
+            {
+                dynamic StratisTicket = JsonConvert.DeserializeObject(client.DownloadString("https://api.coinmarketcap.com/v1/ticker/stratis/"));
+                double StratisPriceUSD = Convert.ToDouble(StratisTicket[0].price_usd);
+                ViewBag.PercentChange = Convert.ToDouble(StratisTicket[0].percent_change_24h);
+                ViewBag.PercentChangeColor = ViewBag.PercentChange < 0 ? "red" : "green";
+
+                switch (CurrencySymbol)
+                {
+                    default:
+                    case "EUR":
+                        ViewBag.Price = (StratisPriceUSD / ConvertEUR_USD()).ToString("C");
+                        break;
+
+                    case "USD":
+                        ViewBag.Price = StratisPriceUSD.ToString("C");
+                        break;
+                }
+            }
+            return PartialView();
+        }
+
+        public double ConvertEUR_USD()
+        {
+            using (var client = new WebClient())
+            {
+                dynamic StratisTicket = JsonConvert.DeserializeObject(client.DownloadString("https://api.fixer.io/latest"));
+                return 1.19985;// Convert.ToDouble(StratisTicket.rates.USD);
+            }
+        }
+    }
+}
